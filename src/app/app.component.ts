2cc32a6183e15beb6579ea6cb45a59da8413c5c7
Shachar Eilenberg <shachar.eilenberg@locusview.com>
2024-09-09 20:27:35 +0300

    angular 17 intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular17-introduction';
+
+  links = [
+    { title: 'Old template syntax', link: 'control-flow-old' },
+    { title: 'New template syntax (control flow)', link: 'control-flow-new' },
+    { title: 'Old way to set loader', link: 'deferrable-views-old' },
+    { title: 'Deferrable views', link: 'deferrable-views-new' },
+    { title: 'Old way to unsubscribe', link: 'unsubscribing-old' },
+    { title: 'New way to unsubscribe', link: 'unsubscribing-new' },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
+import {
+  HasEnhancedFeatureToggle,
+  HasShimonHalisFeatureToggle,
+  IsAuthenticatedGuardService,
+} from '../examples/guards/old-guard.service';
+import {
+  hasFeatureToggleGuard,
+  isAuthenticatedGuard,
+} from '../examples/guards/new-guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'control-flow-old',
+    pathMatch: 'full',
+  },
+  {
+    path: 'control-flow-old',
+    loadComponent: async () => import('../examples/control-flow/old-syntax.component'),
+    canActivate: [IsAuthenticatedGuardService],
+    providers: [IsAuthenticatedGuardService],
+  },
+  {
+    path: 'control-flow-new',
+    loadComponent: async () => import('../examples/control-flow/control-flow.component'),
+    canActivate: [HasEnhancedFeatureToggle],
+    providers: [HasEnhancedFeatureToggle],
+  },
+  {
+    path: 'deferrable-views-old',
+    loadComponent: async () =>
+      import('../examples/deferrable-views/old-way-of-loading.component'),
+    canActivate: [HasShimonHalisFeatureToggle],
+    providers: [HasShimonHalisFeatureToggle],
+  },
+  {
+    path: 'deferrable-views-new',
+    loadComponent: async () =>
+      import('../examples/deferrable-views/deferrable-views.component'),
+    canActivate: [hasFeatureToggleGuard('shimon-halis')],
+  },
+  {
+    path: 'unsubscribing-old',
+    loadComponent: async () =>
+      import('../examples/unsubscribing/old-way-to-unsubscribe-on-destroy.component'),
+    canActivate: [hasFeatureToggleGuard('enhanced')],
+  },
+  {
+    path: 'unsubscribing-new',
+    loadComponent: async () =>
+      import('../examples/unsubscribing/new-way-to-unsubscribe-on-destroy.component'),
+    canActivate: [hasFeatureToggleGuard('enhanced')],
+  },
+];
